473e188d40368ef2227eb768834d555a06971e2a
Chet07-R <178676635+Chet07-R@users.noreply.github.com>
2025-07-03 22:21:58 +0530

    Basic questions on bit-manipulation

---
+++ basics/03-07-25/Q1.cpp
@@ -0,0 +1,15 @@
+// So basically we have an array and we need to find the element which is not repeating or occurs odd number of times in the array.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[]= {1,1,2,2,4,4,5,7,7,8,8};
+    int n= sizeof(arr)/sizeof(arr[0]);   // if we were taking input we will obviously have n
+    int ans=0;
+    for(int i=0;i<n;i++){
+        ans^=arr[i];
+    }
+    cout << ans;
+    return 0;
+}
